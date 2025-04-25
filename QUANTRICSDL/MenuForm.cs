84513ad81b3492f2b564e1e343b58f708c08256f
VNSHuy24 <hvo6471@gmail.com>
2025-04-25 16:37:44 +0700

    xóa db chưa làm được

--- QUANTRICSDL/MenuForm.cs
+++ QUANTRICSDL/MenuForm.cs
@@ -7,22 +7,6 @@
             InitializeComponent();
             FormHelper.SetStandardSize(this);
             // TODO: kết nối đb
-            //this.Load += new System.EventHandler(this.MenuForm_Load);
-
-        }
-
-        private void MenuForm_Load(object sender, EventArgs e)
-        {
-            try
-            {
-                var conn = DatabaseHelper.GetConnection();
-                MessageBox.Show("Kết nối thành công!", "Thông báo");
-                conn.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Lỗi kết nối");
-            }
         }
 
         private void btnQuanLyUserRole_Click(object sender, EventArgs e)
@@ -41,19 +25,12 @@
             this.Show();
         }
 
-    
-
         private void btnXemThongTinQuyen_Click(object sender, EventArgs e)
         {
             ObjectPrivilegesForm objectPrivilegesForm = new ObjectPrivilegesForm();
             this.Hide();
             objectPrivilegesForm.ShowDialog();
             this.Show();
-        }
-
-        private void MenuForm_Load_1(object sender, EventArgs e)
-        {
-
         }
     }
 }
--- QUANTRICSDL/obj/Debug/net8.0-windows/QUANTRICSDL.AssemblyInfo.cs
+++ QUANTRICSDL/obj/Debug/net8.0-windows/QUANTRICSDL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QUANTRICSDL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0977165d50191f68365431af5f6625631c3ced1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f39c876addb08e8680ef7e209861b82fbc98df")]
 [assembly: System.Reflection.AssemblyProductAttribute("QUANTRICSDL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QUANTRICSDL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
