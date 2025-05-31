577d95d19aa06d20db87287220d00df465b424f4
Việt Nguyễn <hungvietbabykute@gmail.com>
2025-05-31 14:17:22 +0700

    OLSSetUp finished

--- QUANTRICSDL/Database/OLSSetUp.sql
+++ QUANTRICSDL/Database/OLSSetUp.sql
@@ -1,4 +1,6 @@
-﻿ALTER SESSION SET CONTAINER = CDB$ROOT;
+﻿-- Connect bằng SYS để cấu hình OLS
+
+ALTER SESSION SET CONTAINER = CDB$ROOT;
 
 BEGIN
     LBACSYS.CONFIGURE_OLS; -- This procedure registers Oracle Label Security. 
@@ -12,11 +14,13 @@ select * from v$services;
 
 ALTER USER lbacsys IDENTIFIED BY lbacsys ACCOUNT UNLOCK;
 ALTER PLUGGABLE DATABASE QLCSDL CLOSE IMMEDIATE; 
-ALTER PLUGGABLE DATABASE QLCSDL OPEN READ WRITE; 
-ALTER SESSION SET CONTAINER= QLCSDL; 
-SHOW CON_NAME; 
+ALTER PLUGGABLE DATABASE QLCSDL OPEN READ WRITE;
 
-CREATE USER ADMIN_OLS IDENTIFIED BY 123 CONTAINER = CURRENT; 
+ALTER SESSION SET CONTAINER= QLCSDL; 
+
+CREATE USER ADMIN_OLS IDENTIFIED BY 123 CONTAINER = CURRENT;
+
+GRANT SELECT, INSERT, UPDATE ON SCHOOL_USER.THONGBAO TO ADMIN_OLS;
 GRANT CONNECT,RESOURCE TO ADMIN_OLS; --CẤP QUYỀN CONNECT VÀ RESOURCE 
 GRANT UNLIMITED TABLESPACE TO ADMIN_OLS; --CẤP QUOTA CHO ADMIN_OLS 
 GRANT SELECT ANY DICTIONARY TO ADMIN_OLS; --CẤP QUYỀN ĐỌC DICTIONARY ---> CẤP QUYỀN EXECUTE CHO ADMIN_OLS 
@@ -40,9 +44,11 @@ BEGIN
   );
 END;
 /
-EXEC SA_SYSDBA.ENABLE_POLICY ('THONGBAO_POLICY'); 
 
--- 3. Tạo các LEVEL, COMPARTMENT, GROUP (chạy bằng admin)
+EXEC SA_SYSDBA.ENABLE_POLICY ('THONGBAO_POLICY'); 
+CONNECT ADMIN_OLS/123@localhost:1521/QLCSDL
+
+-- 3. Tạo các LEVEL, COMPARTMENT, GROUP
 BEGIN
   -- LEVEL
   SA_COMPONENTS.CREATE_LEVEL('THONGBAO_POLICY', 1, 'SINHVIEN', 'Sinh viên');
@@ -61,47 +67,122 @@ BEGIN
 END;
 /
 
-
-CONNECT ADMIN_OLS/123@localhost:1521/QLCSDL
--- 4. Tạo label mẫu (chạy bằng admin)
+-- 4. Tạo label
 BEGIN
-  --SA_LABEL_ADMIN.CREATE_LABEL('THONGBAO_POLICY', 101, 'TRGDV', TRUE);
-  --SA_LABEL_ADMIN.CREATE_LABEL('THONGBAO_POLICY', 102, 'NHANVIEN', TRUE);
-  --SA_LABEL_ADMIN.CREATE_LABEL('THONGBAO_POLICY', 103, 'SINHVIEN', TRUE);
-  SA_LABEL_ADMIN.CREATE_LABEL('THONGBAO_POLICY', 104, 'SVHOACS1', TRUE);
-  SA_LABEL_ADMIN.CREATE_LABEL('THONGBAO_POLICY', 105, 'SVHOACS2', TRUE);
-  SA_LABEL_ADMIN.CREATE_LABEL('THONGBAO_POLICY', 106, 'SVHOABOTH', TRUE);
-  SA_LABEL_ADMIN.CREATE_LABEL('THONGBAO_POLICY', 107, 'TRGDVHANHCHINH', TRUE);
-  SA_LABEL_ADMIN.CREATE_LABEL('THONGBAO_POLICY', 108, 'NVHANHCHINHCS1', TRUE);
-  SA_LABEL_ADMIN.CREATE_LABEL('THONGBAO_POLICY', 109, 'TRGDVHOABOTH', TRUE);
+  -- NHÓM TRƯỞNG ĐƠN VỊ (TRGDV)                    (tag: 100–105)
+  -- u1 / t1: Trưởng đơn vị có thể đọc được toàn bộ thông báo
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 100,
+    label_value => 'TRGDV',
+    data_label  => TRUE
+  );  
+  -- u2: Trưởng đơn vị phụ trách khoa Hóa tại cơ sở 2
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 101,
+    label_value => 'TRGDV:HOA:COSO2',
+    data_label  => TRUE
+  );  
+  -- u3: Trưởng đơn vị phụ trách khoa Lý tại cơ sở 2
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 102,
+    label_value => 'TRGDV:LY:COSO2',
+    data_label  => TRUE
+  );  
+  -- u6: Trưởng đơn vị có thể đọc được các thông báo về Hành chính
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 103,
+    label_value => 'TRGDV:HANHCHINH',
+    data_label  => TRUE
+  );  
+  -- t8: Trưởng khoa Hóa ở cơ sở 1
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 104,
+    label_value => 'TRGDV:HOA:COSO1',
+    data_label  => TRUE
+  );  
+  -- t9: Trưởng khoa Hóa ở cơ sở 1 và cơ sở 2
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 105,
+    label_value => 'TRGDV:HOA:COSO1,COSO2',
+    data_label  => TRUE
+  );
+
+  -- NHÓM NHÂN VIÊN (NHANVIEN)                   (tag: 110–112)
+  -- u7 / t2: Nhân viên có thể đọc toàn bộ thông báo dành cho tất cả nhân viên
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 110,
+    label_value => 'NHANVIEN',
+    data_label  => TRUE
+  );
+  -- u4: Nhân viên thuộc khoa Hóa tại cơ sở 2
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 111,
+    label_value => 'NHANVIEN:HOA:COSO2',
+    data_label  => TRUE
+  );  
+  -- u8: Nhân viên có thể đọc thông báo về Hành chính tại cơ sở 1
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 112,
+    label_value => 'NHANVIEN:HANHCHINH:COSO1',
+    data_label  => TRUE
+  );
+
+  -- NHÓM SINH VIÊN (SINHVIEN)                  (tag: 120–124)
+  -- t3: Cần phát tán đến tất cả sinh viên
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 120,
+    label_value => 'SINHVIEN',
+    data_label  => TRUE
+  );
+  -- u5: Sinh viên khoa Hóa tại cơ sở 2
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 121,
+    label_value => 'SINHVIEN:HOA:COSO2',
+    data_label  => TRUE
+  );
+  -- t4: Cần phát tán đến sinh viên thuộc khoa Hóa ở cơ sở 1
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 122,
+    label_value => 'SINHVIEN:HOA:COSO1',
+    data_label  => TRUE
+  );
+  -- t6: Cần phát tán đến sinh viên thuộc khoa Hóa ở cả hai cơ sở
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 123,
+    label_value => 'SINHVIEN:HOA:COSO1,COSO2',
+    data_label  => TRUE
+  );
+  -- t7: Cần phát tán đến tất cả sinh viên thuộc cả hai cơ sở
+  SA_LABEL_ADMIN.CREATE_LABEL(
+    policy_name => 'THONGBAO_POLICY',
+    label_tag   => 124,
+    label_value => 'SINHVIEN::COSO1,COSO2',
+    data_label  => TRUE
+  );
 END;
 /
-
-DECLARE
-  v_label VARCHAR2(100) := 'TRGDV_ALL';
-BEGIN
-  DBMS_LS.VALIDATE_LABEL(v_label);
-  DBMS_OUTPUT.PUT_LINE('Label is valid.');
-EXCEPTION
-  WHEN OTHERS THEN
-    DBMS_OUTPUT.PUT_LINE('Invalid label: ' || SQLERRM);
-END;
-/
-
-
-SELECT argument_name, data_type, in_out, position
-FROM all_arguments
-WHERE object_name = 'CREATE_LABEL'
-  AND package_name = 'SA_LABEL_ADMIN'
-ORDER BY position;
 
 -- 5. Gán chính sách OLS vào bảng THONGBAO (chạy bằng admin)
-BEGIN
+BEGIN 
   SA_POLICY_ADMIN.APPLY_TABLE_POLICY(
-    policy_name     => 'THONGBAO_POLICY',
-    schema_name     => 'SCHOOL_USER',
-    table_name      => 'THONGBAO',
-    column_name     => 'LABEL_COLUMN'
+    policy_name   => 'THONGBAO_POLICY',
+    schema_name   => 'SCHOOL_USER',
+    table_name    => 'THONGBAO',
+    table_options => 'READ_CONTROL', 
+    predicate     => NULL
   );
 END;
 /
